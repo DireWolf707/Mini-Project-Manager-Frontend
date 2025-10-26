88fc626be52e063ef2d1b89f534ceb327740c6f9
Direwolf <rahulsood707@gmail.com>
2025-10-26 06:26:07 +0530

    added react router and cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
+import { createBrowserRouter, RouterProvider } from "react-router"
 
-createRoot(document.getElementById("root")!).render(<App />)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    // loader: true,
+  },
+])
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+)
